5df86271c07d43a5c994a7b49bee06944fc47613
Fangjie Zhang <fangjie.zhang@shopee.com>
2020-02-19 22:02:18 +0800

    first

---
+++ customer/layers.el
@@ -0,0 +1,8 @@
+;;; layers.el ---                                    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  张方杰
+
+;; Author: 张方杰 <zhangfangjie@BOURNEZHANG-MB0>
+;; Keywords:
+
+(configuration-layer/declare-layer 'gtags)
