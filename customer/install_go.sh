c3f20be893db1214422fc5a8a5344de73f30e7a6
Fangjie Zhang <fangjie.zhang@shopee.com>
2021-02-01 12:06:38 +0800

    add go

---
+++ customer/install_go.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+################################################################################
+#
+# All Rights Reserved
+#
+################################################################################
+#Breif:
+#Authors: zhangfangjie (f22jay@163.com)
+#Date:    2019/08/25 16:27:34
+go get golang.org/x/tools/cmd/goimports
+go get golang.org/x/tools/cmd/godoc
+go get github.com/rogpeppe/godef
+go get golang.org/x/tools/cmd/guru
+go get golang.org/x/lint/golint
+go get github.com/nsf/gocode
+go get golang.org/x/tools/cmd/gorename
